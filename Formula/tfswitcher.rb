2ebca6c567adbedae700be1c1afba23d125b5e58
ASleepyCat <dyeom340@gmail.com>
2023-11-07 19:42:04 +1000

    Build from source

--- Formula/tfswitcher.rb
+++ Formula/tfswitcher.rb
@@ -1,23 +1,20 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Tfswitcher < Formula
   desc "Terraform and OpenTofu version switcher"
   homepage "https://github.com/ASleepyCat/tfswitcher"
-  version "v0.4.1"
+  url "https://github.com/ASleepyCat/tfswitcher/archive/refs/tags/v0.6.0.tar.gz"
+  sha256 "3f09740303075b038d6270b6146b5d53300de75fd2cef01e1da2f8f1e6511644"
   license "MIT"
 
-  on_linux do
-    url "https://github.com/ASleepyCat/tfswitcher/releases/download/#{version}/tfswitcher_#{version}_x86_64-unknown-linux-musl.tar.gz"
-    sha256 "181cfa4275777713bcd5eda7ae09eb2699a280bf9278de0944918d22158e2c14"
-  end
+  depends_on "rust" => :build
 
-  on_macos do
-    url "https://github.com/ASleepyCat/tfswitcher/releases/download/#{version}/tfswitcher_#{version}_x86_64-apple-darwin.zip"
-    sha256 "73c615c3ff250e3ba3251a86d1f7cca0a1d9eded89ca10f0d68f87a9412f451e"
-  end
+  # TODO: Add OpenTofu when it gets added to homebrew-core
+  conflicts_with "terraform", because: "tfswitcher overwrites the current terraform install"
 
   def install
-    bin.install "tfswitcher"
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/tfswitcher", "-V")
   end
 end
